fbe370a1fec80d95eddb0dbedfb72c2a3df12ea5
Rothiii <drome.emord@gmail.com>
2024-07-04 07:38:51 +0800

    fix: add birthdate and password in user also adjustment in seed file

--- prisma/seed/adminDefault.ts
+++ prisma/seed/adminDefault.ts
@@ -10,4 +10,15 @@ export const adminDefault = [
     department_id: 1,
     role_id: 1,
   },
+  {
+    full_name: "admin2",
+    phone_number: "081159599",
+    email: "11211040@student.itk.ac.id",
+    password: "admin123",
+    company_id: 1,
+    job_position_id: 1,
+    employment_status_id: 2,
+    department_id: 1,
+    role_id: 2,
+  }
 ];
--- prisma/seed/jobPositionDefault.ts
+++ prisma/seed/jobPositionDefault.ts
@@ -1,5 +1,17 @@
 export const jobPositionDefault = [
   {
+    name: "Direktur",
+  },
+  {
+    name: "Supervisor",
+  },
+  {
+    name: "Manager",
+  },
+  {
+    name: "Staff",
+  },
+  {
     name: "Jabatan Lainnya",
   },
 ];
