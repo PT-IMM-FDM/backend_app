d1b48de471e7732feeb282a137e6dc4e5cf6a3b7
Rothiii <drome.emord@gmail.com>
2024-08-09 11:11:17 +0800

    feat: utils for parse array params of number

--- src/features/fitDailyMonitoring/fdmController.ts
+++ src/features/fitDailyMonitoring/fdmController.ts
@@ -1,43 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { FdmService } from "./fdmService";
 import { ErrorResponse } from "../../models";
-
-const parseArrayParam = (param: any): number[] | undefined => {
-  if (!param) return undefined;
-
-  if (Array.isArray(param)) {
-    // Jika param adalah array, konversikan setiap elemen menjadi angka
-    return param
-      .map((item) => {
-        const num = parseInt(item);
-        if (isNaN(num)) {
-          console.warn(`Warning: "${item}" cannot be converted to a number`);
-        }
-        return num;
-      })
-      .filter((num) => !isNaN(num));
-  } else if (typeof param === "string") {
-    // Jika param adalah string, ubah menjadi array angka
-    return param
-      .split(",")
-      .map((item) => {
-        const num = parseInt(item.trim());
-        if (isNaN(num)) {
-          console.warn(`Warning: "${item}" cannot be converted to a number`);
-        }
-        return num;
-      })
-      .filter((num) => !isNaN(num));
-  }
-
-  // Jika param bukan string atau array, langsung konversikan menjadi angka tunggal
-  const num = parseInt(param);
-  if (isNaN(num)) {
-    console.warn(`Warning: "${param}" cannot be converted to a number`);
-    return undefined;
-  }
-  return [num];
-};
+import { parseArrayParam } from "../../utils";
 
 export class FdmController {
   static async getFDM(req: Request, res: Response, next: NextFunction) {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./hash";
 export * from "./format";
 export * from "./whatsapp-message"
+export * from "./parse_array_param"
