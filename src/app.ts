77fcf481fa808646a6828d0ee04b1b069f20614d
Rothiii <drome.emord@gmail.com>
2024-11-09 14:58:14 +0800

    feat: add /public endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cron from "node-cron";
+import path from "path";
 import { clientWhatsapp, sendMessageFdmUnfit } from "./utils";
 import { ErrorMiddleware } from "./middlewares";
 import { loggerMiddleware } from "./middlewares/loggerCLI";
@@ -22,6 +23,8 @@ app.use(loggerMiddleware); // Middleware morgan but custom
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use("/public", express.static(path.join(__dirname, "../public")));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
